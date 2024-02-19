1207cbddfb6c040da53dd954838a23a2238ea88f
Stanislav <s.tyulkov@gmail.com>
2024-02-19 13:02:36 +0300

    hw10

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Задача 1");
+        String firstName = "Ivan";
+        String middleName = "Ivanov";
+        String lastName = "Ivanovich";
+        String fullName = middleName + " " + firstName + " " + lastName;
+        System.out.println("Ф. И. О. сотрудника — " + fullName);
+
+        System.out.println("Задача 2");
+        fullName = fullName.toUpperCase();
+        System.out.println("Данные Ф. И. О. сотрудника для заполнения отчета — " + fullName);
+
+        System.out.println("Задача 3");
+        fullName = fullName.replace("IVANOV IVAN IVANOVICH", "Иванов Семён Семёнович");
+        fullName = fullName.replace('ё', 'е');
+        System.out.println("Данные Ф. И. О. сотрудника — " + fullName);
     }
 }
